c14b0c4f01e379ff218f29f6278e59501025d09e
yuanyongjun <1195029042@qq.com>
2017-07-31 22:31:22 +0800

    finish

---
+++ js/index.js
@@ -0,0 +1,119 @@
+﻿/**
+ * Created by Mac on 2017/1/7.
+ */
+function $(id) {
+    //判断id的类型
+    return typeof id === 'string'?document.getElementById(id):id;
+}
+
+//当网页加载完毕
+window.onload = function () {
+    //瀑布流布局 保证传的参数能够找到父盒子
+    waterFall('main','box');
+
+    //滚动加载盒子
+    window.onscroll = function ()
+    {
+        //判断是否加载
+        if (checkWillLoad())
+        {
+            //创造假数据
+            var data = {'dataImg':[{'img':'1.jpg'},{'img':'2.jpg'},{'img':'3.jpg'},{'img':'4.jpg'},{'img':'5.jpg'},{'img':'6.jpg'}]};
+            //加载数据
+            for(var i=0; i<data.dataImg.length; i++)
+            {
+                //创建最外面的盒子
+                var newBox = document.createElement('div');
+                newBox.className = 'box';
+                $('main').appendChild(newBox);
+                //创建单个盒子
+                var newPic = document.createElement('div');
+                newPic.className = 'pic';
+                newBox.appendChild(newPic);
+                //创建img
+                var newImg = document.createElement('img');
+                newImg.src = './imges/' + data.dataImg[i].img;
+                newPic.appendChild(newImg);
+            }
+            //把刚创建的盒子瀑布流布局
+            waterFall('main','box');
+        }
+    }
+}
+
+//实现瀑布流布局
+//规则:从第二行开始的图片,总是拼接在上一行高度最矮的图片后面
+function  waterFall(parent,box) {
+    //父盒子居中
+    //通过父盒子拿到所有的子盒子
+    var allBox = $(parent).getElementsByClassName(box);
+    //求出盒子的宽度
+    var boxWidth = allBox[0].offsetWidth;
+    //求出浏览器的宽度(包括边框的宽高)
+    var screenWidtn = document.body.offsetWidth;
+    //求出列数 //取整函数取整
+    var cols = Math.floor( screenWidtn/boxWidth);
+    //父标签居中
+    //先求出父标签宽度
+    $(parent).style.width = boxWidth * cols + 'px';
+    //居中
+    $(parent).style.margin = '0 auto';
+
+    //子盒子定位
+    //创建一个高度数组,存所有的高度
+    var heightArr = [];
+    //遍历
+    for(var i = 0; i < allBox.length ;i++)
+    {
+        //求出每个盒子的高度
+        var boxHeight = allBox[i].offsetHeight;
+        //第一行的盒子不需要重新定位//每一行的盒子数与列数相同
+        if(i<cols)
+        {
+            //添加第一行所有盒子高度
+            heightArr.push(boxHeight);
+        }
+        else//剩下的盒子都需要瀑布流布局
+        {
+            //求出最矮的盒子高度
+            var minBoxHeight = Math.min.apply(this,heightArr);
+            //求出最矮盒子对应的索引
+            var minBoxIndex = getMinBoxIndex(minBoxHeight,heightArr);
+            //盒子瀑布流定位  顶部间距就是最矮盒子的高度
+            allBox[i].style.position = 'absolute';
+            allBox[i].style.top = minBoxHeight + 'px';
+            allBox[i].style.left = minBoxIndex * boxWidth +'px';
+            //关键:更新数组最矮高度,使下一个图片在高度数组中总是找最矮高度的图片下面拼接
+            heightArr[minBoxIndex] += boxHeight;
+        }
+    }
+}
+
+//求出最矮盒子对应的索引函数
+function getMinBoxIndex(val,arr) {
+    for(var i in arr)
+    {
+        if(val == arr[i])
+        {
+            return i;
+        }
+    }
+}
+
+//加载更多规则:当浏览器最下方到达图片的高度一半时让其刷新出来
+//判断是否符合加载条件
+function checkWillLoad() {
+    //取出所有盒子
+    var allBox = $('main').getElementsByClassName('box');
+    //取出最后一个盒子
+    var lastBox = allBox[allBox.length - 1];
+    //求出最后一个盒子高度的一半 + 内容与浏览器头部的偏离位置
+    var lastBoxDis = lastBox.offsetHeight * 0.5 + lastBox.offsetTop;
+    //求出浏览器的高度
+    // 注意:JS代码存在浏览器兼容问题 一般分标准模式(按屏幕算document.body.offsetHeight)和混杂模式(按所有内容算)
+    var screenHeight =  document.documentElement.clientHeight;
+    //页面偏离屏幕的高度
+    var scrollTopHeight = document.body.scrollTop;
+    //判断
+    return lastBoxDis <= screenHeight + scrollTopHeight;
+}
---
+++ js/index2.js
@@ -0,0 +1,85 @@
+﻿$(window).on('load',function () {
+    //1.实现瀑布流布局
+    waterFall();
+
+    //2.滚动加载
+    $(window).on('scroll',function () {
+        //判断是否加载
+        if (checkWillLoad())
+        {
+            ////创造假数据
+            var data = {'dataImg':[{'img':'1.jpg'},{'img':'2.jpg'},{'img':'3.jpg'},{'img':'4.jpg'},{'img':'5.jpg'},{'img':'6.jpg'}]};
+            //遍历创建盒子
+            $.each(data.dataImg,function (index,value)
+                   {
+                       //创建一个div标签 设置它的类为'box' 添加到'main'里面去
+                       var newBox = $('<div>').addClass('box').appendTo($('#main'));
+                       var newPic = $('<div>').addClass('pic').appendTo($(newBox));
+                       //创建img  取出遍历的对象value的img属性对应的值
+                       $('<img>').attr('src','imges/'+$(value).attr('img')).appendTo($(newPic));
+                   })
+            //1.实现瀑布流布局
+            waterFall();
+        }
+    });
+});
+
+//实现瀑布流布局
+function waterFall () {
+    //拿到所有的盒子
+    var allBox = $('#main > .box');
+    //取出其中一个盒子的宽度
+    var boxWidth = $(allBox).eq(0).outerWidth();
+    //取出屏幕的高度
+    var screenWidth = $(window).width();
+    //求出列数 //取整函数取整
+    var cols = Math.floor( screenWidth/boxWidth);
+    //父标签居中
+    $('#main').css({
+        'width':cols * boxWidth + 'px',
+        'margin':'0 auto'
+    });
+    //对子盒子定位
+    var heightArr = [];
+    //遍历
+    $.each(allBox,function (index,value) {
+        //取出单独盒子的高度
+        var boxHeight = $(value).outerHeight();
+        //判断是否第一行
+        if(index < cols)
+        {
+            heightArr[index] = boxHeight;
+        }
+        else  //剩余的盒子要瀑布流布局
+        {
+            //求出最矮的盒子高度
+            var minBoxHeight = Math.min.apply(null,heightArr);
+            //取出最矮高度对应的索引  封装了js的这个方法
+            var minBoxIndex = $.inArray(minBoxHeight,heightArr);
+            //定位
+            $(value).css({
+                'position':'absolute',
+                'top':minBoxHeight + 'px',
+                'left':minBoxIndex * boxWidth + 'px'
+            });
+            //更新数组中最矮的高度
+            heightArr[minBoxIndex] += boxHeight;
+        }
+    })
+
+}
+
+//判断是否符合加载条件
+function checkWillLoad() {
+    //直接取出最后一个盒子
+    var lastBox = $('#main > div').last();
+    //取出最后一个盒子高度的一半 + 头部偏离的位置
+    var lastBoxDis = $(lastBox).outerHeight() + $(lastBox).offset().top;
+    //求出浏览器的高度
+    var clientHeight = $(window).height();
+    //求出页面偏离浏览器高度
+    var scrollTopHeight = $(window).scrollTop();
+    //比较返回
+    return lastBoxDis <= clientHeight + scrollTopHeight;
+
+}
